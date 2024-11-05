bee39acd95e829f3e97a8fd86d09b0fe43d4e03c
Sergey Lihobabin <sergey.lihobabin@gmail.com>
2024-11-05 12:21:45 +0500

    getCookie function added

--- helpers.ts
+++ helpers.ts
@@ -1,4 +1,4 @@
-const convertBase64 = (blob: Blob) => {
+export function convertBase64(blob: Blob){
     return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
         fileReader.readAsDataURL(blob);
@@ -12,3 +12,19 @@ const convertBase64 = (blob: Blob) => {
         };
     });
 };
+
+export function getCookie(cname: string) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
