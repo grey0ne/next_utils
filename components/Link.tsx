4cd468f03d487c25068b14c856b0bd3f7484105f
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-05-24 17:59:02 +0300

    added unlocalized noprefetch link

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Link as NextLink } from '@/next_utils/i18n/navigation';
+import { Link as NextIntlLink } from '@/next_utils/i18n/navigation';
+import NextLink from 'next/link';
 
 export const NoPrefetchLink = React.forwardRef(
+    (props: any, ref:React.ForwardedRef<any>) => <NextIntlLink {...props} ref={ ref } prefetch={props.prefetch ?? false}/>
+)
+NoPrefetchLink.displayName = 'NoPrefetchLink';
+
+export const UnlocalizedLink = React.forwardRef(
     (props: any, ref:React.ForwardedRef<any>) => <NextLink {...props} ref={ ref } prefetch={props.prefetch ?? false}/>
 )
 NoPrefetchLink.displayName = 'NoPrefetchLink';
