8f64d8859b58fd7fc902c3ffcd92c71c0718b428
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-07-07 18:59:12 +0500

    Locale selector appearance improved

--- components/LocaleSelector.tsx
+++ components/LocaleSelector.tsx
@@ -1,41 +1,71 @@
 'use client'
 import { TextLink } from "@/next_utils/components/Link"
-import { Box, Select, MenuItem } from "@mui/material"
+import { Box, Button, MenuItem, Popover } from "@mui/material"
 import { usePathname } from '@/next_utils/i18n/navigation';
 import { useLocale } from "next-intl";
+import { useState } from "react";
 import { AVAILABLE_LOCALES } from "@/next_utils/constants"; 
 import { ENABLED_LOCALES } from "@/constants";
 import Image from "next/image"
 
 export function LocaleSelector() {
+    const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+        event.stopPropagation();
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
     const url = usePathname();
     const locale = useLocale();
 
     const filteredLocales = AVAILABLE_LOCALES.filter(l => ENABLED_LOCALES.includes(l.code));
 
     return (
-        <Select
-            value={locale}
-            sx={{ color: 'inherit' }}
-            IconComponent={() => null} // Hide default icon
-            variant="outlined"
-        >
-            {filteredLocales.map((locale) => (
-                <MenuItem key={locale.code} value={locale.code}>
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                        <Image
-                            src={locale.flag}
-                            alt={locale.label}
-                            width={20}
-                            height={15}
-                            style={{ objectFit: 'contain' }}
-                        />
-                        <TextLink href={ url } locale={locale.code }>
-                            {locale.label}
-                        </TextLink>
-                    </Box>
-                </MenuItem>
-            ))}
-        </Select>
+        <>
+            <Button variant="text" color="inherit" onClick={handleClick}>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                    <Image
+                        src={filteredLocales.find(l => l.code === locale)?.flag}
+                        alt={locale}
+                        width={20}
+                        height={15}
+                        style={{ objectFit: 'contain' }}
+                    />
+                    {filteredLocales.find(l => l.code === locale)?.shortLabel}
+                </Box>
+            </Button>
+            <Popover
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                {filteredLocales.map((locale) => (
+                    <MenuItem key={locale.code} value={locale.code}>
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                            <Image
+                                src={locale.flag}
+                                alt={locale.label}
+                                width={20}
+                                height={15}
+                                style={{ objectFit: 'contain' }}
+                            />
+                            <TextLink href={ url } locale={locale.code }>
+                                {locale.label}
+                            </TextLink>
+                        </Box>
+                    </MenuItem>
+                ))}
+            </Popover>
+        </>
     );
 }
