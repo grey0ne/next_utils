015b25a2fed1db297cd4ba62858e64e141562721
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-07-20 20:49:20 +0500

    harcoded locales to improve compatibility with unlocalized projects

--- types.ts
+++ types.ts
@@ -1,5 +1,10 @@
-import { components } from '@/api/apiTypes';
+export type LocalizedString = {
+  ru?: string;
+  de?: string;
+  en?: string;
+  es?: string;
+  pt?: string;
+}
 
-export type BackendLocalizedString = components['schemas']['LocalizedStringSchema']
-export type LocalizedString = components['schemas']['LocalizedStringSchema']
+export type BackendLocalizedString = LocalizedString //TODO dynamically configure Locale from backend
 export type BackendLocale = keyof BackendLocalizedString
