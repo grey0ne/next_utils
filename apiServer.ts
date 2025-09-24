ceff7aa23ab690e179647987bf5a16fadca328ad
Sergey Likhobabin <sergey.lihobabin@gmail.com>
2025-09-24 23:17:51 +0300

    added anonymous backend requests without cookies for static page generation

--- apiServer.ts
+++ apiServer.ts
@@ -18,6 +18,46 @@ export const apiGet = async <P extends Path>(
     return apiRequest(url, 'get', {}, ...params); 
 }
 
+/**
+ * 
+ * Server-side API Get function that does not require authentication. Used for static cached routes
+ */
+export const anonymousApiGet = async <P extends Path>(
+    url: P,
+    ...params: RequestParams<P, 'get'> extends undefined ? [] : [RequestParams<P, 'get'>]
+): Promise<{ errors: string[], data: ResponseType<P, 'get'> | null}> => {
+    return anonymousApiRequest(url, 'get', {}, ...params); 
+}
+
+function getFormattedUrl<P extends Path>(url: P, params: RequestParams<P, 'get'> extends undefined ? [] : [RequestParams<P, 'get'>]) {
+    const pathParams = params[0]?.path;
+    const queryParams = params[0]?.query;
+    return BACKEND_URL + generateUrl(url, pathParams, queryParams); 
+}
+
+export const anonymousApiRequest = async <P extends Path, M extends PathMethod<P>>(
+    url: P,
+    method: M,
+    body: RequestBody<P, M> extends undefined ? object : RequestBody<P, M>,
+    ...params: RequestParams<P, M> extends undefined ? [] : [RequestParams<P, M>]
+): Promise<{ errors: string[], data: ResponseType<P, M> | null}> => {
+    const formattedUrl = getFormattedUrl(url, params); 
+    const response =  await fetch(
+        formattedUrl, {
+            method: String(method),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            cache: 'no-store',
+        }
+    );
+    if (response.ok) {
+        const result = await response.json()
+        return { errors: [], data: result };
+    } else {
+        return { errors: [`Response status ${response.status}`], data: null }
+    }
+}
 
 export const apiRequest = async <P extends Path, M extends PathMethod<P>>(
     url: P,
@@ -25,9 +65,7 @@ export const apiRequest = async <P extends Path, M extends PathMethod<P>>(
     body: RequestBody<P, M> extends undefined ? object : RequestBody<P, M>,
     ...params: RequestParams<P, M> extends undefined ? [] : [RequestParams<P, M>]
 ): Promise<{ errors: string[], data: ResponseType<P, M> | null}> => {
-    const pathParams = params[0]?.path;
-    const queryParams = params[0]?.query;
-    const formattedUrl = BACKEND_URL + generateUrl(url, pathParams, queryParams); 
+    const formattedUrl = getFormattedUrl(url, params); 
     const cookieStore = await cookies();
     const cookieHeader = cookieStore.getAll()
         .map(cookie => `${cookie.name}=${cookie.value}`)
